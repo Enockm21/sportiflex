546f35db2a4bad58e107a503c06632ad7c1326a4
Enock MUKOKO MAYA <emukoko@planetworkint.com>
2024-04-16 15:47:35 +0200

    initialisation projet

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold">
+         start sporti flex 
+       </h1>
+      
+      </div>
+      
+    </>
+  )
+}
+
+export default App
